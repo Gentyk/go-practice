a3f8e35f9135105ae49e51621f80b6ec537ab1ac
v <33gva@mail.ru>
2026-04-01 15:35:34 +0300

    add ozon task

--- real_questions/friends/ozon.go
+++ real_questions/friends/ozon.go
@@ -1,25 +1,65 @@
-package friends
+package main
 
 import (
+	"crypto/md5"
 	"fmt"
 )
 
-var alf = []rune{'a', 'b', 'c'}
+var alphabet = []rune{'a', 'b', 'c', 'd', '1', '2', '3'}
 
-func find(length1 int, target string) string {
+func RecoverPassword(h []byte) string {
+	for i := 1; i <= len(h); i++ {
+		password := find(i, h)
+		if password != "" {
+			return password
+		}
+	}
+	return ""
+}
+
+// func TestRecoverPassword(t *testing.T) {
+// 	for _, exp := range []string{
+// 		"a",
+// 		"12",
+// 		"abc333d",
+// 	} {
+// 		t.Run(exp, func(t *testing.T) {
+// 			act := RecoverPassword(hashPassword(exp))
+// 			if act != exp {
+// 				t.Error("recovered:", act, "expected:", exp)
+// 			}
+// 		})
+// 	}
+// }
+
+func hashPassword(in string) []byte {
+	h := md5.Sum([]byte(in))
+	return h[:]
+}
+
+func check(h []byte, password string) bool {
+	newPass := hashPassword(password)
+
+	for i, v := range newPass {
+		if v != h[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func find(length1 int, h []byte) string {
 	current := make([]rune, length1)
 
 	var recurs func(pos int) bool
 
 	recurs = func(pos int) bool {
 		if pos == length1 {
-			if string(current) == target {
-				return true
-			}
-			return false
+			return check(h, string(current))
 		}
 
-		for _, i := range alf {
+		for _, i := range alphabet {
 			current[pos] = i
 			if recurs(pos + 1) {
 				return true
@@ -34,4 +74,17 @@ func find(length1 int, target string) string {
 	}
 
 	return ""
+}
+
+func main() {
+	for _, exp := range []string{
+		"a",
+		"12",
+		"abc333d",
+	} {
+		act := RecoverPassword(hashPassword(exp))
+		if act != exp {
+			fmt.Println("recovered:", act, "expected:", exp)
+		}
+	}
 }
