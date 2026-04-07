16d199c9ad667cb57e54baa2b3371ed7b1ccb719
v <33gva@mail.ru>
2026-04-07 17:56:19 +0300

    add trace_id

--- practice/for_vk/domain/data.go
+++ practice/for_vk/domain/data.go
@@ -1,10 +1,17 @@
 package domain
 
 import (
+	"context"
+	"log/slog"
 	"time"
 )
 
-func GetData() string {
+func GetData(ctx context.Context) string {
+	if logger, ok := ctx.Value("logger").(*slog.Logger); ok {
+		logger.InfoContext(ctx, "GetData running")
+	} else {
+		slog.InfoContext(ctx, "GetData running")
+	}
 	time.Sleep(10 * time.Millisecond)
 	return "data"
 }
--- practice/for_vk/main.go
+++ practice/for_vk/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"log/slog"
+	"os"
 	"practice/for_vk/domain"
 )
 
 func main() {
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level:     slog.LevelInfo,
+		AddSource: true,
+	})
+	slog.SetDefault(slog.New(handler))
+
+	
+
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -13,8 +25,11 @@ func main() {
 		})
 	})
 	router.GET("/data", func(c *gin.Context) {
+		traceID := uuid.NewString()
+		logger := slog.Default().With("trace_id", traceID)
+		ctx := context.WithValue(c.Request.Context(), "logger", logger)
 		c.JSON(200, gin.H{
-			"message": domain.GetData(),
+			"message": domain.GetData(ctx),
 		})
 	})
 	router.Run() // listens on 0.0.0.0:8080 by default
